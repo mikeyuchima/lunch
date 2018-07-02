ee5d1257290b5a3fb9f6bf60be3c60af9e434cbe
Michael Surya <tomichaels@gmail.com>
2018-07-02 17:46:33 +0000

    Here is your lunch logic

--- lunch.js
+++ lunch.js
@@ -14,7 +14,20 @@
  */
 
 function whatToDoForLunch(hungry, availableTime) {
-  console.log("I don't know what to do!");
+  if (hungry == true) {
+  	if (availableTime <= 20) {
+  		console.log("pick something up and eat in back in the Lab or in the kitchen, where you can get to know your fellow classmates.");
+  	} else if (availableTime > 20 && availableTime < 30) {
+  		console.log("because you've been working hard and are ahead of schedule, you deserve a break and could try a place in Chi-Town.");
+  	} else if (availableTime > 30) {
+  		console.log("this is a bootcamp after all and you should probably reconsider.");
+  	}
+  } else if (hungry == false) {
+  	console.log("keep at it!")
+  }
+  else {
+  	console.log("I Can't Do That");
+  }
 }
 
 
